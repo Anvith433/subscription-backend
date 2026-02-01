27043de4b3564d5665291fa75faf9c52482644b5
anvit <anvithshetty932@gmail.com>
2026-02-01 23:44:57 +0530

    RecommendationRepository is the file added and written to access the data objects(DAO's)

---
+++ src/main/java/com/yourorg/Recommendation/RecommendationRepository.java
@@ -0,0 +1,16 @@
+package com.yourorg.Recommendation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+    Optional<Recommendation> findByTitle(String title);
+    Optional<Recommendation> findById(long id);
+    Recommendation save(Recommendation recommendation);
+
+    Recommendation deleteById(int id);
+
+    
+    
+}

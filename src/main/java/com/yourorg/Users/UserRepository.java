91ff623ab53de355bc59d50329ecbd8745bd480f
anvit <anvithshetty932@gmail.com>
2026-02-01 22:36:46 +0530

    repository file added to connect it to database

---
+++ src/main/java/com/yourorg/Users/UserRepository.java
@@ -0,0 +1,13 @@
+package com.yourorg.Users;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+    User save(User user);
+    User deleteById(int id);
+     User findById(long id);
+    
+}
